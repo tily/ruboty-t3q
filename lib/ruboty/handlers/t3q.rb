99498f2a39ad6c3ebc4940b33b2f1e978e39bf8b
tily <tidnlyam@gmail.com>
2015-04-14 21:11:20 +0900

    add debug print

--- lib/ruboty/handlers/t3q.rb
+++ lib/ruboty/handlers/t3q.rb
@@ -53,6 +53,7 @@ module Ruboty
         in_reply_to_status_id = message.original[:tweet].try(:in_reply_to_status_id)
         if book = data.find {|k, v| v['title_status_id'] == in_reply_to_status_id }
           response = tumblr.quote(tumblr_blog_name, quote: message[:text], source: book.first)
+	  p response
           dm "tumblr に投稿しました #{response["posts"]["post_url"]}"
 	end
       end
